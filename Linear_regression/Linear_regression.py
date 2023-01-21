9752ff2dddc310ccf516a2727a3bee156fb18c2e
Shivam singh <shivamsingh8496@gmail.com>
2023-01-21 20:03:14 +0530

    Loading the presexixting model

--- Linear_regression/Linear_regression.py
+++ Linear_regression/Linear_regression.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn  import datasets , linear_model
 from sklearn.metrics import mean_squared_error
+import pickle
 
 diabates = datasets.load_diabetes()
 cols =['data', 'target', 'frame', 'DESCR', 'feature_names', 'data_filename', 'target_filename', 'data_module'] 
@@ -18,6 +19,13 @@ diabates_y_test = diabates_x[-20:]
 model = linear_model.LinearRegression() 
 model.fit(diabates_x_train,diabates_y_train)
 
+with open("linearmodel.pickle","wb") as f:
+    pickle.dump(model,f)                            #Saving the daata
+
+# pickle_in =  open("linearmodel.pickle","rb")
+# model = pickle.load(pickle_in)                    #Loading the data
+
+
 diabates_y_predict = model.predict(diabates_x_test)
 
 print("Mean squared error :" , mean_squared_error(diabates_y_test,diabates_y_predict))
@@ -32,3 +40,5 @@ plt.show()
 # Mean squared error : 1.8178056230099e-33
 # weights : [[1.]]
 #  Intercept : [-9.48676901e-20]
+
+#  1.3945098721799241e-33
